73269eb72ed2b34e87a9408fd5126f4e175605e1
ZavaryginAndrey <andrey.zavarygin@gmail.com>
2021-01-19 14:58:49 +0200

    Initialize clear source files

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <the-navbar></the-navbar>
+  <div class="container with-nav">
+    <Tasks />
+  </div>
+</template>
+
+<script>
+import TheNavbar from './components/TheNavbar'
+import Tasks from './views/Tasks'
+
+export default {
+  components: {
+    TheNavbar,
+    Tasks
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/TheNavbar.vue
@@ -0,0 +1,19 @@
+<template>
+  <header class="navbar">
+    <h3>Freelance</h3>
+
+    <ul class="navbar-menu">
+      <li>
+        <a href="#">Все задачи</a>
+      </li>
+      <li>
+        <a href="#">Создать</a>
+      </li>
+    </ul>
+  </header>
+</template>
+
+<script>
+export default {
+}
+</script>
